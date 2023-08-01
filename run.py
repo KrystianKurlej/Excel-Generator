12ffc92767a0d5caf44e2c7fb1d84ba19032a704
KrystianKurlej <101930877+KrystianKurlej@users.noreply.github.com>
2023-08-01 15:21:00 +0200

    ending program

--- run.py
+++ run.py
@@ -19,30 +19,37 @@ def take_input(question):
 
 
 if __name__ == "__main__":
-    print("Wybierz opcję:")
-    print("1. Generowanie Excela")
-    print("2. Dodawanie uczestnika")
-    print("3. Usuwanie uczestnika")
+    while True:
+        print("Wybierz opcję:")
+        print("1. Generowanie Excela")
+        print("2. Dodawanie uczestnika")
+        print("3. Usuwanie uczestnika")
+        print("4. Zakończ program")
 
-    choice = take_input("Podaj numer opcji: ")
+        choice = take_input("Podaj numer opcji: ")
 
-    if choice == 1:
-        try:
-            # Uruchomienie skryptu create_excel.py
-            subprocess.run(["python3", "scripts/create_excel.py"], check=True)
-        except subprocess.CalledProcessError:
-            print("Wystąpił błąd podczas uruchamiania skryptu tworzącego arkusz.")
-    elif choice == 2:
-        try:
-            # Uruchomienie skryptu add_member.py
-            subprocess.run(["python3", "scripts/add_member.py"], check=True)
-        except subprocess.CalledProcessError:
-            print("Wystąpił błąd podczas uruchamiania skryptu dodawania uczestnika.")
-    elif choice == 3:
-        try:
-            # Uruchomienie skryptu delete_member.py
-            subprocess.run(["python3", "scripts/delete_member.py"], check=True)
-        except subprocess.CalledProcessError:
-            print("Wystąpił błąd podczas uruchamiania skryptu usuwania uczestnika.")
-    else:
-        print("Niepoprawny numer opcji. Wybierz 1 lub 2.")
+        if choice == 1:
+            try:
+                # Uruchomienie skryptu create_excel.py
+                subprocess.run(["python3", "scripts/create_excel.py"], check=True)
+            except subprocess.CalledProcessError:
+                print("Wystąpił błąd podczas uruchamiania skryptu tworzącego arkusz.")
+        elif choice == 2:
+            try:
+                # Uruchomienie skryptu add_member.py
+                subprocess.run(["python3", "scripts/add_member.py"], check=True)
+            except subprocess.CalledProcessError:
+                print(
+                    "Wystąpił błąd podczas uruchamiania skryptu dodawania uczestnika."
+                )
+        elif choice == 3:
+            try:
+                # Uruchomienie skryptu delete_member.py
+                subprocess.run(["python3", "scripts/delete_member.py"], check=True)
+            except subprocess.CalledProcessError:
+                print("Wystąpił błąd podczas uruchamiania skryptu usuwania uczestnika.")
+        elif choice == 4:
+            print("Zamykam program.")
+            break
+        else:
+            print("Niepoprawny numer opcji. Wybierz 1, 2, 3 lub 4.")
